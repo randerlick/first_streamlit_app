9071b1c298254590ac072cb2e111a354b323e63a
randerlick <151088970+randerlick@users.noreply.github.com>
2023-11-16 16:36:59 -0600

    Update streamlit_app.py
    Added special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
 streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
